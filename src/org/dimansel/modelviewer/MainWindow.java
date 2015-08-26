c8a0e2e51c84f16d2beb7a8064891e2ee72ec249
Дмитрий <assassin00000@mail.ru>
2015-08-26 17:16:41 +0300

    Initial commit

---
+++ src/org/dimansel/modelviewer/MainWindow.java
@@ -0,0 +1,123 @@
+package org.dimansel.modelviewer;
+
+import org.dimansel.projection3d.Keyboard;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+
+public class MainWindow extends JFrame implements KeyEventDispatcher, ActionListener {
+    public final int WIDTH = 800;
+    public final int HEIGHT = 800;
+
+    public Screen screen;
+    private Robot robot;
+    private Point prevPoint = new Point(); //storing previous mouse position
+    private long lastFPS;
+    private int fps;
+
+    public MainWindow() {
+        //INITIALIZATION
+        screen = new Screen(800, 800);
+        lastFPS = System.currentTimeMillis();
+
+        //ADDING LISTENERS
+        KeyboardFocusManager kfm = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+        kfm.addKeyEventDispatcher(this);
+        Timer timer = new Timer(10, this);
+        timer.start();
+
+        //WINDOW CONFIGURATION
+        setSize(WIDTH, HEIGHT);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation((screenSize.width - WIDTH) / 2, (screenSize.height - HEIGHT) / 2);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Model Viewer");
+        add(screen);
+        setVisible(true);
+        try { robot = new Robot(); } catch(AWTException e) {e.printStackTrace();}
+    }
+
+    private void render() {
+        handleMouse(); //rotate camera
+        screen.cam.processKeyboard(0.1); //moving camera
+        screen.projectVertices();
+        screen.repaint();
+        updateFPS();
+    }
+
+    private void setCursor() {
+        if (screen.mouseGrabbed) {
+            BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+            Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(
+                    cursorImg, new Point(0, 0), "blank cursor");
+            getContentPane().setCursor(blankCursor);
+        } else {
+            getContentPane().setCursor(Cursor.getDefaultCursor());
+        }
+    }
+
+    private void handleMouse() {
+        Point p = new Point(WIDTH/2, HEIGHT/2);
+        SwingUtilities.convertPointToScreen(p, screen);
+        Point e = MouseInfo.getPointerInfo().getLocation();
+
+        if ((!e.equals(p) && screen.mouseGrabbed)) {
+            screen.cam.processMouse(e.x - prevPoint.x, e.y - prevPoint.y);
+            prevPoint = e;
+        }
+
+        if (screen.mouseGrabbed) {
+            robot.mouseMove(p.x, p.y);
+            prevPoint = p;
+        }
+    }
+
+    private void grabMouse() {
+        Point p = new Point(WIDTH/2, HEIGHT/2);
+        SwingUtilities.convertPointToScreen(p, screen);
+        prevPoint = p;
+        robot.mouseMove(p.x, p.y);
+        screen.mouseGrabbed = !screen.mouseGrabbed;
+    }
+
+    private void updateFPS() {
+        if (System.currentTimeMillis() - lastFPS > 1000) {
+            setTitle("FPS: " + String.valueOf(fps));
+            fps = 0;
+            lastFPS += 1000;
+        }
+        fps++;
+    }
+
+    /**
+     * EVENTS
+     */
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent e) {
+        if (e.getID() == KeyEvent.KEY_PRESSED) {
+            Keyboard.addKey(e.getKeyCode());
+            if (e.getKeyCode() == KeyEvent.VK_G) {
+                grabMouse();
+                setCursor();
+            }
+        }
+        if (e.getID() == KeyEvent.KEY_RELEASED) Keyboard.removeKey(e.getKeyCode());
+
+        return false;
+    }
+
+    /**
+     * Timer tick
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        render();
+    }
+
+    public static void main(String[] args) {
+        new MainWindow();
+    }
+}
---
+++ src/org/dimansel/modelviewer/Screen.java
@@ -0,0 +1,66 @@
+package org.dimansel.modelviewer;
+
+import org.dimansel.math3d.Vertex3D;
+import org.dimansel.projection3d.Camera;
+import org.dimansel.projection3d.Model;
+import org.dimansel.projection3d.OBJModelLoader;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Screen extends JPanel {
+    protected Camera cam;
+    protected ArrayList<Model> models;
+    private int width, height;
+    protected boolean mouseGrabbed = false;
+
+    public Screen(int width, int height) {
+        setSize(width, height);
+        setBackground(Color.BLACK);
+        this.width = width;
+        this.height = height;
+
+        init();
+    }
+
+    private void init() {
+        models = new ArrayList<>();
+        cam = new Camera(width, height, 70, 0, 100);
+
+        Model model1 = OBJModelLoader.load("C:\\Users\\freak\\Desktop\\Models\\suzanne.obj", 1);
+        model1.color = new Color(255, 255, 255);
+        model1.position = new Vertex3D(0, 0, 5);
+        //model1.gouraudShading = true;
+        models.add(model1);
+    }
+
+    protected void projectVertices() {
+        for (Model m : models) {
+            m.projectVertices(cam, cam.pos);
+        }
+    }
+
+    private void renderModels(Graphics g) {
+        BufferedImage frameBuffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        int[] data = ((DataBufferInt)frameBuffer.getRaster().getDataBuffer()).getData();
+        double[] zbuffer = new double[width*height];
+        Arrays.fill(zbuffer, 100);
+
+        for (Model m : models) {
+            m.Render(data, zbuffer, width);
+        }
+
+        g.drawImage(frameBuffer, 0, 0, this);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+
+        renderModels(g);
+    }
+}
